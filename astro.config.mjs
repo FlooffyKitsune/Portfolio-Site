b4bf1f4a034307476e1c0ee2cacb2d9d4429164e
FlooffyKitsune <wafflekat1123@gmail.com>
2026-08-06 14:22:37 +0900

    chore: scaffold Astro, remove SvelteKit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+	integrations: [svelte()],
+	vite: {
+		plugins: [Icons({ compiler: 'svelte' })]
+	}
+});
